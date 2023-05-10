37eca2114805ac1aed2e2b3068cc378d1f7da9cd
Olena911 <olenaazulay@gmail.com>
2023-05-10 12:18:34 +0300

    Added github repo Url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       Weather app
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/olena-azulay-365213262/" target="_blank"
+              rel ="noopener noreferrer">Olena Azulay</a>  and is{" "}
+      <a href="https://github.com/Olena911/react-home" target="_blank"
+              rel ="noopener noreferrer">
+open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
