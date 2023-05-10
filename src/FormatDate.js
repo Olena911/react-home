c16a5bfe42ea322b32eb18840925d4c81b3cb3fa
Olena911 <olenaazulay@gmail.com>
2023-05-10 15:59:51 +0300

    updated FormatDate

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default function FormatDate(props){
-    
+    console.log(props.date);
     let days= ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    let day= days[props.date.getDay()];
-    let hours= props.date.getHours();
+    let day= days[props.data.getDay()];
+    let hours= props.data.getHours();
     if (hours < 10){
         hours=`0${hours}`;
     }
-    let minutes= props.date.getMinutes();
+    let minutes= props.data.getMinutes();
     if(minutes < 10){
         minutes=`0${minutes}`;
     }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,7 +7,7 @@ export default function WeatherInfo(props){
         <div className="WeatherInfo">
 <h1>{props.data.city}</h1>
         <ul>
-            ///
+         <li><FormatDate data={props.data.date} /> </li>
            
             <li className="text-capitalize">{props.data.description} </li>
         </ul>
